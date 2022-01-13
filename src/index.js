dd328bbbbe7f68fcb20c0cf384a640773ae0ce4a
Nick Sullivan <nicholassull@gmail.com>
2022-01-13 15:48:39 -0800

    add document.ready() to index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './js/currency-service';
+
+$(document).ready(function() {
+
+});
