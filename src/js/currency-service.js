cd88c4d861bcb2b9acdd4e83690b72ce05b1569d
Nick Sullivan <nicholassull@gmail.com>
2022-01-13 15:45:23 -0800

    add currency-service.js and development environment files

---
+++ src/js/currency-service.js
@@ -0,0 +1,14 @@
+export default class CurrencyService {
+  static getData() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+  }
+}
