ef6dfecb5d503ba6c05e0ff843b3cef2c790a756
Nick Sullivan <nicholassull@gmail.com>
2022-01-13 16:19:22 -0800

    add css styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyService from './js/currency-service';
+// import CurrencyService from './js/currency-service';
 
 $(document).ready(function() {
 
