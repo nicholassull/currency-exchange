ff67ded952e6d88caf3334860551a05a8a1ae942
Nick Sullivan <nicholassull@gmail.com>
2022-01-13 16:09:14 -0800

    add more intial HTML <div>s

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {
   static getData() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
