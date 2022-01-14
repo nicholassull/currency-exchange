87b218049b193e1aa724b5452eac91a23d604f1f
Nick Sullivan <nicholassull@gmail.com>
2022-01-13 17:19:14 -0800

    added sessionStorage function after API call

--- src/index.js
+++ src/index.js
@@ -2,8 +2,33 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import CurrencyService from './js/currency-service';
+import CurrencyService from './js/currency-service';
+
+// 1 : NZD : New Zealand Dollar
+// 2 : INR : Indian Roupee
+// 3 : JPY : Japanese Yen
+// 4 : MXN : Mexican Peso
+// 5 : ZAR : South Agrican Rand
+
+function storeData(response) {
+  if (response.result) {
+    sessionStorage.setItem(1, response.conversion_rates.NZD);
+    sessionStorage.setItem(2, response.conversion_rates.INR);
+    sessionStorage.setItem(3, response.conversion_rates.JPY);
+    sessionStorage.setItem(4, response.conversion_rates.MXN);
+    sessionStorage.setItem(5, response.conversion_rates.ZAR);
+  }
+}
 
 $(document).ready(function() {
+  CurrencyService.getData()
+    .then(function(response) {
+      storeData(response);
+    }).then(function(response){
+      console.log(sessionStorage.getItem(1));
+    });
+  $('#exchange-btn').click(function() {
+    let currency = $('#exchange-select').val();
 
+  });
 });
--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,4 +1,7 @@
 export default class CurrencyService {
+
+
+
   static getData() {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
@@ -12,3 +15,4 @@ export default class CurrencyService {
       })
   }
 }
+
